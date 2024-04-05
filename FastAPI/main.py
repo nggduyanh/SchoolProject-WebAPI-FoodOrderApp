770b8d035a9dcfa04f58e0b97152f92772dbd81c
nggduyanh <ahihihi115@gmail.com>
2024-04-05 15:20:34 +0700

    feat(frontend):create base

--- FastAPI/main.py
+++ FastAPI/main.py
@@ -22,13 +22,13 @@ app.add_middleware(
 
 models.Base.metadata.create_all(bind=engine)
 
-class PostBase(BaseModel):
-    title: str
-    content: str
-    user_id: int
+# class PostBase(BaseModel):
+#     title: str
+#     content: str
+#     user_id: int
 
-class UserBase(BaseModel):
-    username: str
+# class UserBase(BaseModel):
+#     username: str
 
 def get_db():
     db=SessionLocal()
@@ -39,68 +39,68 @@ def get_db():
 
 db_dependency=Annotated[Session,Depends(get_db)]
 
-@app.get("/users/",status_code=status.HTTP_200_OK)
-async def getall_user(db:db_dependency):
-    user = db.query(models.User).all()
-    if user is None:
-        raise HTTPException(status_code=404,detail='Dont have any user')
-    return user
+# @app.get("/users/",status_code=status.HTTP_200_OK)
+# async def getall_user(db:db_dependency):
+#     user = db.query(models.User).all()
+#     if user is None:
+#         raise HTTPException(status_code=404,detail='Dont have any user')
+#     return user
 
-@app.get("/users/{user_id}",status_code=status.HTTP_200_OK)
-async def get_user_by_id(user_id:int,db:db_dependency):
-    user = db.query(models.User).filter(models.User.id==user_id).first()
-    if user is None:
-        raise HTTPException(status_code=404,detail='User not found')
-    return user
+# @app.get("/users/{user_id}",status_code=status.HTTP_200_OK)
+# async def get_user_by_id(user_id:int,db:db_dependency):
+#     user = db.query(models.User).filter(models.User.id==user_id).first()
+#     if user is None:
+#         raise HTTPException(status_code=404,detail='User not found')
+#     return user
 
-@app.post("/users/",status_code=status.HTTP_201_CREATED)
-async def post_user(user:UserBase,db:db_dependency):
-    db_user=models.User(**user.dict())
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
+# @app.post("/users/",status_code=status.HTTP_201_CREATED)
+# async def post_user(user:UserBase,db:db_dependency):
+#     db_user=models.User(**user.dict())
+#     db.add(db_user)
+#     db.commit()
+#     db.refresh(db_user)
 
-@app.put("/users/{user_id}",status_code=status.HTTP_200_OK)
-async def put_user(user_id:int,user:UserBase,db:db_dependency):
-    db_user=db.query(models.User).filter(models.User.id==user_id).first()
-    db_user=user
-    db.commit()
-    db.refresh(db_user)
+# @app.put("/users/{user_id}",status_code=status.HTTP_200_OK)
+# async def put_user(user_id:int,user:UserBase,db:db_dependency):
+#     db_user=db.query(models.User).filter(models.User.id==user_id).first()
+#     db_user=user
+#     db.commit()
+#     db.refresh(db_user)
 
-@app.delete("/user/{user_id}",status_code=status.HTTP_200_OK)
-async def delete_user_by_id(user_id:int,db:db_dependency):
-    user=db.query(models.User).filter(models.User.id==user_id).first()
-    if user is None:
-        raise HTTPException(status_code=404,detail="Post not found")
-    db.delete(user)
-    db.commit()
+# @app.delete("/user/{user_id}",status_code=status.HTTP_200_OK)
+# async def delete_user_by_id(user_id:int,db:db_dependency):
+#     user=db.query(models.User).filter(models.User.id==user_id).first()
+#     if user is None:
+#         raise HTTPException(status_code=404,detail="Post not found")
+#     db.delete(user)
+#     db.commit()
 
-@app.get("/posts/",status_code=status.HTTP_200_OK)
-async def getall_post(db:db_dependency):
-    post=db.query(models.Post).all()
-    if post is None:
-        raise HTTPException(status_code=404,detail="Post not found")
-    return post
+# @app.get("/posts/",status_code=status.HTTP_200_OK)
+# async def getall_post(db:db_dependency):
+#     post=db.query(models.Post).all()
+#     if post is None:
+#         raise HTTPException(status_code=404,detail="Post not found")
+#     return post
 
-@app.get("/posts/{post_id}",status_code=status.HTTP_200_OK)
-async def get_post_by_id(post_id:int,db:db_dependency):
-    post=db.query(models.Post).filter(models.Post.id==post_id).first()
-    if post is None:
-        raise HTTPException(status_code=404,detail="Post not found")
-    return post
+# @app.get("/posts/{post_id}",status_code=status.HTTP_200_OK)
+# async def get_post_by_id(post_id:int,db:db_dependency):
+#     post=db.query(models.Post).filter(models.Post.id==post_id).first()
+#     if post is None:
+#         raise HTTPException(status_code=404,detail="Post not found")
+#     return post
 
-@app.post("/posts/",status_code=status.HTTP_201_CREATED)
-async def post_post(post:PostBase,db:db_dependency):
-    db_post=models.Post(**post.dict())
-    db.add(db_post)
-    db.commit()
-    db.refresh(db_post)
+# @app.post("/posts/",status_code=status.HTTP_201_CREATED)
+# async def post_post(post:PostBase,db:db_dependency):
+#     db_post=models.Post(**post.dict())
+#     db.add(db_post)
+#     db.commit()
+#     db.refresh(db_post)
 
-@app.delete("/post/{post_id}",status_code=status.HTTP_200_OK)
-async def delete_post(post_id:int,db:db_dependency):
-    post=db.query(models.Post).filter(models.Post.id==post_id).first()
-    if post is None:
-        raise HTTPException(status_code=404,detail="Post not found")
-    db.delete(post)
-    db.commit()
-    db.refresh(post)
+# @app.delete("/post/{post_id}",status_code=status.HTTP_200_OK)
+# async def delete_post(post_id:int,db:db_dependency):
+#     post=db.query(models.Post).filter(models.Post.id==post_id).first()
+#     if post is None:
+#         raise HTTPException(status_code=404,detail="Post not found")
+#     db.delete(post)
+#     db.commit()
+#     db.refresh(post)
